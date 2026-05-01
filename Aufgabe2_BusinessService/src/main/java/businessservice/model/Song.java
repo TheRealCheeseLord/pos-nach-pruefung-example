52bb914bb51223b52d5c42019d05675ff92e9f11
Sima Baric <sima.baric0605@gmail.com>
2026-05-01 16:20:22 +0200

    Provided stub setup

---
+++ Aufgabe2_BusinessService/src/main/java/businessservice/model/Song.java
@@ -0,0 +1,7 @@
+package businessservice.model;
+
+public record Song(
+        String title,
+        String artist,
+        String genre
+) {}
---
+++ Aufgabe2_BusinessService/src/main/java/businessservice/service/GraduationPartyPlaylistService.java
@@ -0,0 +1,15 @@
+package businessservice.service;
+
+import businessservice.model.Song;
+
+import java.util.List;
+import java.util.Map;
+
+public class GraduationPartyPlaylistService {
+
+    Map<String, List<Song>> preparePlaylist(
+            List<List<Song>> studentPlaylists
+    ) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
